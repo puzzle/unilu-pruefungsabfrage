9ecb8989c2c363636adfb1816eff041f4be71e33
peggimann <peggimann@puzzle.com>
2024-08-07 13:35:54 +0200

    set up spring boot application

---
+++ src/main/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplication.java
@@ -0,0 +1,13 @@
+package ch.puzzle.exam_feedback_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFeedbackToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFeedbackToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.puzzle.exam_feedback_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFeedbackToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
