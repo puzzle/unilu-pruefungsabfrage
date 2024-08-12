89dad896a279384610068c79f1255b2499d76dc4
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-12 12:59:37 +0200

    Return to correctly formatted code

--- src/test/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplicationTests.java
+++ src/test/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ExamFeedbackToolApplicationTests {
 
-@Test
-void contextLoads() {
-}
+    @Test
+    void contextLoads() {
+    }
 
 }
