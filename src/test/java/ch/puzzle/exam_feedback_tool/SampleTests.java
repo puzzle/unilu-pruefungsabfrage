932ccf0eff7fb2205eff74794a6d4930b7b38488
Manuel <109959820+ManuelMoeri@users.noreply.github.com>
2024-08-12 13:56:07 +0200

    Feature/5 CI Pipelines erststellen für Unit Test und E2E Tests (#21)
    * Add test-action file and define rough workflow
    
    * Change distribution and name
    
    * Add test that fails to check pipeline
    
    * Rename failingtest to sampletests and add a dummy-test for running the pipeline
    
    * Format code and fix naming error

---
+++ src/test/java/ch/puzzle/exam_feedback_tool/SampleTests.java
@@ -0,0 +1,16 @@
+package ch.puzzle.exam_feedback_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SampleTests {
+
+    @Test
+    void pipelineShouldRun() {
+        int number = 1;
+        assertEquals(1, number);
+    }
+}
