d6d1c584959d9db392a9e8d4bfe23e0e043071af
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-12 16:06:50 +0200

    Import formatting template and format code with new style

--- src/main/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplication.java
+++ src/main/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExamFeedbackToolApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExamFeedbackToolApplication.class, args);
+        SpringApplication
+                .run(ExamFeedbackToolApplication.class, args);
     }
 
 }
