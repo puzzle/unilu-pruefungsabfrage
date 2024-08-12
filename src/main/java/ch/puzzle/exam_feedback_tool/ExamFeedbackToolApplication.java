da9278042baee53372c4f582625bb3fa041554fe
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-12 10:44:55 +0200

    Include formatter plugin and format code

--- src/main/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplication.java
+++ src/main/java/ch/puzzle/exam_feedback_tool/ExamFeedbackToolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamFeedbackToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamFeedbackToolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExamFeedbackToolApplication.class, args);
+    }
 
 }
