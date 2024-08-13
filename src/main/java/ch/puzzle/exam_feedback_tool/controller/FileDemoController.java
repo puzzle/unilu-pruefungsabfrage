9fc9b2b581fbe7a37085aff5f6638b9f25f84599
digennaro <digennaro@puzzle.ch>
2024-08-13 15:37:08 +0200

    add intelij run config and update dockerfuile

--- src/main/java/ch/puzzle/exam_feedback_tool/controller/FileDemoController.java
+++ src/main/java/ch/puzzle/exam_feedback_tool/controller/FileDemoController.java
@@ -1,26 +1,27 @@
 package ch.puzzle.exam_feedback_tool.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/v2")
 public class FileDemoController {
+    @Value("${basePath}")
+    private String basePath;
 
     @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public ResponseEntity<FileSystemResource> downloadFile(HttpServletResponse response) {
-        System.out.println("Download file");
-        System.out.println(new File(".").getAbsolutePath());
-        File examFile = new File("/resources/11111_11112222.pdf");
+//        System.out.println(Arrays.stream(new File(basePath).listFiles()).map(File::getAbsoluteFile).toList());
+        File examFile = new File(basePath + "/11111_11112222.pdf");
         response.setHeader("Content-Disposition", "attachment; filename=" + "test" + ".pdf");
-//        if (!examFile.exists())
-//            return ResponseEntity.notFound().build();
         return ResponseEntity.ok(new FileSystemResource(examFile));
     }
 }
