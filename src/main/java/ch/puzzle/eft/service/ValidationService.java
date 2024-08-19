46cd768dc8f055c710a96fc1101ec7c1b4269841
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-19 11:55:20 +0200

    Implement first version of validation service to validate exam number and matrikel number

--- src/main/java/ch/puzzle/eft/service/ValidationService.java
+++ src/main/java/ch/puzzle/eft/service/ValidationService.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Service;
 public class ValidationService {
     public boolean validateExamNumber(String examNumber) {
         String examNumberRegex = "^[0-9]{5}$";
-        return examNumber.matches(examNumberRegex);
+        return examNumber
+                .matches(examNumberRegex);
     }
 
     public boolean validateMatrikelNumber(String matrikelNumber) {
         String matrikelNumberRegex = "^[0-9]{8}$";
-        return matrikelNumber.matches(matrikelNumberRegex);
+        return matrikelNumber
+                .matches(matrikelNumberRegex);
     }
 }
