0cbdf468dd0bc8ffd2d6065bd495a2c330f4c19a
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-20 12:35:16 +0200

    Add controller test to assure the default route returns the index page

---
+++ src/test/java/ch/puzzle/eft/controller/SiteControllerTest.java
@@ -0,0 +1,31 @@
+package ch.puzzle.eft.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(SiteController.class)
+@WithMockUser
+public class SiteControllerTest {
+    MockMvc mockMvc;
+
+    @Autowired
+    public SiteControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void defaultRouteShouldReturnIndexPage() throws Exception {
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(status()
+                        .isOk())
+                .andExpect(view()
+                        .name("index"));
+    }
+}
