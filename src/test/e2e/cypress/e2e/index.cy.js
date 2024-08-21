5d0f9ebf9aa8eabd25aa968057e542ee9303834a
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-21 08:12:49 +0200

    Put cypress visit command into before each

--- src/test/e2e/cypress/e2e/index.cy.js
+++ src/test/e2e/cypress/e2e/index.cy.js
@@ -1,13 +1,14 @@
-it('should show greeting message on index page', () => {
+beforeEach(() => {
     cy.visit("/");
+})
 
+it('should show greeting message on index page', () => {
     cy.get('body').then((body) => {
         expect(body).to.contain("Willkommen bei der Prüfungsabfrage der Uni Luzern")
     })
 })
 
 it('should display header on index page', () => {
-    cy.visit("/");
     cy.get('header').then((header) => {
         expect(header).to.contain("Uni Luzern Prüfungsabfrage")
     })
