3f39d20b976f3e0d10446e2965a0b093566cde90
Jannik Pulfer <109959802+RandomTannenbaum@users.noreply.github.com>
2024-08-21 08:36:00 +0200

    Feature/22 validation service (#34)
    * Implement first version of validation service to validate exam number and matrikel number
    
    * Implement first version of validation service to validate exam number and matrikel number
    
    * Write tests to test any possible user inputted exam numbers that should not be matched by the validator
    
    * Add regex itself to tested string values of validation test
    
    * Rename test in validation service tests
    
    * Make exam number validation use precompiled regex, make it null safe and test the null safety

---
+++ src/main/java/ch/puzzle/eft/service/ValidationService.java
@@ -0,0 +1,22 @@
+package ch.puzzle.eft.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class ValidationService {
+    private static final Pattern EXAM_NUMBER_PATTERN = Pattern
+            .compile("^[0-9]{5}$");
+
+    public boolean validateExamNumber(String examNumber) {
+        if (examNumber == null) {
+            return false;
+        }
+        Matcher matcher = EXAM_NUMBER_PATTERN
+                .matcher(examNumber);
+        return matcher
+                .matches();
+    }
+}
