c89030ea167192d0d8299b898ffcfde7d3841575
Jannik Pulfer <pulfer@puzzle.ch>
2024-08-21 08:12:49 +0200

    Put cypress visit command into before each

--- src/test/e2e/cypress/e2e/search.cy.js
+++ src/test/e2e/cypress/e2e/search.cy.js
@@ -1,13 +1,14 @@
-it('should show search page', () => {
+beforeEach(() => {
     cy.visit("/search");
+})
 
+it('should show search page', () => {
     cy.get('body').then((body) => {
         expect(body).to.contain("Suche nach einer Prüfung...")
     })
 })
 
 it('should display header on search page', () => {
-    cy.visit("/search");
     cy.get('header').then((header) => {
         expect(header).to.contain("Uni Luzern Prüfungsabfrage")
     })
