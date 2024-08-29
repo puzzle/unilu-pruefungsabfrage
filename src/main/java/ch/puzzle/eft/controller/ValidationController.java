95353fe6ad93ac968b36ee95f5e573aca9dc113a
digennaro <digennaro@puzzle.ch>
2024-08-29 16:08:20 +0200

    Make input stay after submitting

--- src/main/java/ch/puzzle/eft/controller/ValidationController.java
+++ src/main/java/ch/puzzle/eft/controller/ValidationController.java
@@ -3,15 +3,17 @@ package ch.puzzle.eft.controller;
 import ch.puzzle.eft.model.ExamNumberForm;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ValidationController {
 
     @PostMapping("/validate")
-    public String viewValidatePage(@Valid ExamNumberForm examNumberForm, BindingResult bindingResult) {
+    public String viewValidatePage(@Valid ExamNumberForm examNumberForm, BindingResult bindingResult, Model model) {
+        model
+                .addAttribute("examNumberForm", examNumberForm);
         return "search";
     }
 }
