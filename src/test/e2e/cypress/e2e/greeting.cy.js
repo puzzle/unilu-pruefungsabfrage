9edf620b1668d8b51364ada5881999ebc4f476d0
Miguel Lehmann <lehmann@puzzle.ch>
2024-08-29 15:03:01 +0200

    delete demo objects

--- src/test/e2e/cypress/e2e/greeting.cy.js
+++
@@ -1,7 +0,0 @@
-it('should show greeting according to query param on greeting page', () => {
-    cy.visit("/greeting?name=Adrian");
-
-    cy.get('body').then((body) => {
-        expect(body).to.contain("Hello Adrian")
-    })
-})
