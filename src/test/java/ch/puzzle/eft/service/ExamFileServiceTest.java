5bf954d72df3a1312ef30a2114816c06f8108d99
Manuel <109959820+ManuelMoeri@users.noreply.github.com>
2024-09-03 16:14:54 +0200

    Feature/12 exam reading service (#51)
    * Add service and base model for reading files
    
    * Add logging and returning of empty list for validation and add test
    
    * change naming of matriculation number
    
    * put file directory in environment variable
    
    * change get property so null gets returned if nothing is found
    
    ---------
    
    Co-authored-by: Miguel Lehmann <lehmann@puzzle.ch>

---
+++ src/test/java/ch/puzzle/eft/service/ExamFileServiceTest.java
@@ -0,0 +1,129 @@
+package ch.puzzle.eft.service;
+
+import ch.puzzle.eft.model.ExamFileModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ExamFileServiceTest {
+
+    @Spy
+    ExamFileService examFileService;
+
+    @BeforeEach
+    void setUp() {
+        when(examFileService
+                .getBasePath())
+                .thenReturn("static");
+    }
+
+    @Autowired
+    public ExamFileServiceTest(ExamFileService examFileService) {
+        this.examFileService = examFileService;
+    }
+
+    @Test
+    void shouldReturnAllExamFiles() {
+
+        List<File> result = examFileService
+                .getAllExamFiles();
+
+        List<File> filesToCheck = List
+                .of(
+                    Paths
+                            .get("static", "Handels und Gesellschaftsrecht", "11000_11112222.pdf")
+                            .toFile(), Paths
+                                    .get("static", "Privatrecht", "11000_11112222.pdf")
+                                    .toFile(), Paths
+                                            .get("static", "Strafrecht", "11000_11112222.pdf")
+                                            .toFile(), Paths
+                                                    .get("static", "Öffentliches Recht", "11000_11112222.pdf")
+                                                    .toFile()
+                );
+
+        assertEquals(22, result
+                .size());
+        for (File fileToCheck : filesToCheck) {
+            assertTrue(result
+                    .contains(fileToCheck));
+        }
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDirectoryIsEmpty() {
+        when(examFileService
+                .getSubjectDirectories())
+                .thenReturn(new File[0]);
+        ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class, () -> examFileService
+                .getAllExamFiles());
+        assertEquals(HttpStatus.NOT_FOUND, responseStatusException
+                .getStatusCode());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDirectoryIsNonExistent() {
+        when(examFileService
+                .getBasePath())
+                .thenReturn("nonExistentDirectory");
+        ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class, () -> examFileService
+                .getAllExamFiles());
+        assertEquals(HttpStatus.NOT_FOUND, responseStatusException
+                .getStatusCode());
+    }
+
+    @Test
+    void shouldReturnMatchingExamNamesAndAmount() {
+        List<String> filesToCheck = List
+                .of(
+                    "static/Handels und Gesellschaftsrecht/11001_22223333.pdf", "static/Privatrecht/11001_22223333.pdf", "static/Strafrecht/11001_22223333.pdf", "static/Öffentliches Recht/11001_22223333.pdf"
+                );
+
+        List<String> expectedFileNames = filesToCheck
+                .stream()
+                .map(p -> Paths
+                        .get(p)
+                        .toFile())
+                .map(ExamFileModel::new)
+                .map(ExamFileModel::getFileName)
+                .toList();
+
+        List<ExamFileModel> result = examFileService
+                .getMatchingExams("11001", "22223333");
+
+        List<String> resultFileNames = result
+                .stream()
+                .map(ExamFileModel::getFileName)
+                .toList();
+
+        assertEquals(resultFileNames, expectedFileNames);
+        assertIterableEquals(expectedFileNames, resultFileNames, "The lists of exam file names should match");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenExamNumberInputIsInvalid() {
+        ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class, () -> examFileService
+                .getMatchingExams("53", "44445555"));
+        assertEquals(HttpStatus.BAD_REQUEST, responseStatusException
+                .getStatusCode());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNoMatchingExamsAreFound() {
+        ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class, () -> examFileService
+                .getMatchingExams("11004", "22223333"));
+        assertEquals(HttpStatus.NOT_FOUND, responseStatusException
+                .getStatusCode());
+    }
+}
