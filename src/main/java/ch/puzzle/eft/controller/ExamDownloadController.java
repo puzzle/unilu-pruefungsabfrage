6e4c66edbb1bd16ce8db261cec0a68eede8ed146
Manuel <moeri@puzzle.ch>
2024-09-06 14:47:05 +0200

    Add missing controller annotation

--- src/main/java/ch/puzzle/eft/controller/ExamDownloadController.java
+++ src/main/java/ch/puzzle/eft/controller/ExamDownloadController.java
@@ -1,7 +1,9 @@
 package ch.puzzle.eft.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ExamDownloadController {
 
     @GetMapping("/download/")
