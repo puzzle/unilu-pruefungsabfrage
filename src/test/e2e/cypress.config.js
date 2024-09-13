c131af647ddf13a2a0ec5ea97eafd46630ba9141
Manuel <moeri@puzzle.ch>
2024-09-13 16:06:13 +0200

    Add E2E tests for naming and add task in cypress-config to delete the downloads folder

--- src/test/e2e/cypress.config.js
+++ src/test/e2e/cypress.config.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
+      on('task', {
+        clearDownloads() {
+          const downloadsFolder = path.join(__dirname, 'cypress/downloads');
+
+          fs.readdir(downloadsFolder, (err, files) => {
+            if (err) {
+              console.log('Could not list the directory.', err);
+              return null;
+            }
+
+            files.forEach((file) => {
+              const filePath = path.join(downloadsFolder, file);
+              fs.unlink(filePath, (unlinkErr) => {
+                if (unlinkErr) {
+                  console.log('Error deleting file:', unlinkErr);
+                }
+              });
+            });
+          });
+
+          return null;
+        },
+      });
     },
     baseUrl: 'http://localhost:8080',
-    downloadsFolder: 'cypress/downloads'
+    downloadsFolder: 'cypress/downloads',
   },
 };
