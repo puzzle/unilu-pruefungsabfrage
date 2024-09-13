ef885345dde71a16ab789469d50090afafbc08c0
Manuel <moeri@puzzle.ch>
2024-09-13 11:25:01 +0200

    Fix tests and add cypress/downloads folder to gitignore

--- src/main/java/ch/puzzle/eft/controller/ExamDownloadController.java
+++ src/main/java/ch/puzzle/eft/controller/ExamDownloadController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ import java.io.File;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/exams")
 public class ExamDownloadController {
     ExamFileService examFileService;
 
