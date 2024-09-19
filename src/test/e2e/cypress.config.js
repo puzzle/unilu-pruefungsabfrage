18c6616be12a676bafe924535aadc28ed6679700
Manuel <109959820+ManuelMoeri@users.noreply.github.com>
2024-09-19 11:55:40 +0200

    Bug/85 Error message in Cypress logs (#87)
    * Fix tests and remove cleanupDownload task

--- src/test/e2e/cypress.config.js
+++ src/test/e2e/cypress.config.js
@@ -1,34 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      on('task', {
-        clearDownloads() {
-          const downloadsFolder = path.join(__dirname, 'cypress/downloads');
-
-          fs.readdir(downloadsFolder, (err, files) => {
-            if (err) {
-              console.log('Could not list the directory.', err);
-              return null;
-            }
-
-            files.forEach((file) => {
-              const filePath = path.join(downloadsFolder, file);
-              fs.unlink(filePath, (unlinkErr) => {
-                if (unlinkErr) {
-                  console.log('Error deleting file:', unlinkErr);
-                }
-              });
-            });
-          });
-
-          return null;
-        },
-      });
+    e2e: {
+        baseUrl: 'http://localhost:8080',
+        downloadsFolder: 'cypress/downloads',
     },
-    baseUrl: 'http://localhost:8080',
-    downloadsFolder: 'cypress/downloads',
-  },
 };
--- src/test/e2e/cypress/e2e/search.cy.js
+++ src/test/e2e/cypress/e2e/search.cy.js
@@ -1,6 +1,5 @@
 beforeEach(() => {
     cy.visit("/search");
-    cy.task('clearDownloads');
 })
 
 it('should show info message on search page', () => {
@@ -53,11 +52,15 @@ it('should not display ZIP download when exam number is invalid', () => {
     cy.get('Alle als ZIP herunterladen').should('not.exist');
 });
 
-it('should download file when link is clicked', () => {
+it('should download files with correct name when link is clicked', () => {
     cy.get('input[type="text"]').type('11000');
     cy.get('button').click();
-    cy.contains('Privatrecht').click();
-    cy.readFile('cypress/downloads/Privatrecht.pdf').should('exist');
+    const subjects = ['Handels und Gesellschaftsrecht', 'Privatrecht', 'Strafrecht']
+
+    for (let i = 0; i < subjects.length; i++) {
+        cy.contains(subjects[i]).click();
+        cy.readFile(`cypress/downloads/${subjects[i]}.pdf`).should('exist');
+    }
 });
 
 it('should show downloadable files with name of subject-folder they are in', () => {
@@ -67,19 +70,6 @@ it('should show downloadable files with name of subject-folder they are in', () 
     cy.contains('Privatrecht');
     cy.contains('Strafrecht');
     cy.contains('Öffentliches Recht')
-});
-
-it('should rename files to subject-folder they are inside of after downloading', () => {
-    cy.get('input[type="text"]').type('11000');
-    cy.get('button').click();
-    const subjects = ['Handels und Gesellschaftsrecht', 'Privatrecht', 'Strafrecht']
-
-    let i
-    for (i = 0; i < subjects.length; i++) {
-        cy.contains(subjects[i]).click();
-        cy.readFile(`cypress/downloads/${subjects[i]}.pdf`).should('exist');
-    }
-    cy.expect(i).to.equal(3)
 });
 
 it('should not be disabled if number is in input', () => {
