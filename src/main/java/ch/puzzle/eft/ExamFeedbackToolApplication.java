a38f133c63ffd06dbf08ee688bc8b52267fc985e
Yanick Minder <minder@puzzle.ch>
2024-09-23 14:06:50 +0200

    use layouts and style basics

--- src/main/java/ch/puzzle/eft/ExamFeedbackToolApplication.java
+++ src/main/java/ch/puzzle/eft/ExamFeedbackToolApplication.java
@@ -1,13 +1,20 @@
 package ch.puzzle.eft;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamFeedbackToolApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExamFeedbackToolApplication.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }
