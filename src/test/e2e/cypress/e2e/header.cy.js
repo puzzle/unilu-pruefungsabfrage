a5fd8ee64081306212df559ec84f5f48f7e26959
Manuel <moeri@puzzle.ch>
2024-09-26 14:37:37 +0200

    Cleanup E2E-Tests

--- src/test/e2e/cypress/e2e/header.cy.js
+++ src/test/e2e/cypress/e2e/header.cy.js
@@ -3,18 +3,35 @@ beforeEach(() => {
     cy.get('[data-testid="cookie-consent-button"]').click();
 })
 
-it('should display the right image', () => {
-    cy.get("header").find("#logo")
-        .should('have.attr', 'src')
-        .and('include', 'UNILU_Schriftzug_Standard_schwarz_DE.png');
-});
+describe('header displays all elements', () => {
+    it('should display the right image', () => {
+        cy.get("header").find("#logo")
+            .should('have.attr', 'src')
+            .and('include', 'UNILU_Schriftzug_Standard_schwarz_DE.png');
 
-it('should navigate to /search when link to the Search Page is clicked', () => {
-    const item = cy.get('header').contains('Prüfungslaufnummer')
-        item.should('exist');
-    item.click();
-    cy.url().should('eq', Cypress.config().baseUrl + '/search');
-    cy.get('body').then((body) => {
-        expect(body).to.contain("Suche nach einer Prüfung...")
+    });
+
+    it('should display navbar', () => {
+        cy.get('header').then((header) => {
+            expect(header).to.contain("Prüfungslaufnummer")
+            expect(header).to.contain("Kontakt")
+            expect(header).to.contain("Logout")
+        })
     })
-});
+})
+
+describe('header provides excepted functionality', () => {
+    it('should navigate to /search when link to the Search Page is clicked', () => {
+        const item = cy.get('header').contains('Prüfungslaufnummer')
+        item.should('exist');
+        item.click();
+        cy.url().should('eq', Cypress.config().baseUrl + '/search');
+        cy.get('body').then((body) => {
+            expect(body).to.contain("Suche nach einer Prüfung...")
+        })
+    });
+
+    // ToDo: Write tests for 'Kontakt' and 'Logout'
+})
+
+
--- src/test/e2e/cypress/e2e/search.cy.js
+++ src/test/e2e/cypress/e2e/search.cy.js
@@ -2,97 +2,98 @@ beforeEach(() => {
     cy.visit("/search");
 })
 
-it('should show info message on search page', () => {
-    cy.get('body').then((body) => {
-        expect(body).to.contain("Suche nach einer Prüfung...")
+describe('search site displays all static elements', () => {
+    it('should show info message on search page', () => {
+        cy.get('body').then((body) => {
+            expect(body).to.contain("Suche nach einer Prüfung...")
+        })
     })
+
+    it('should display header on search page', () => {
+        cy.get('#logo').should("be.visible");
+    })
+
+    it('should display label for searchbar', () => {
+        cy.get('body').then((body) => {
+            cy.contains("Prüfungsnummer eingeben:")
+        })
+    });
+
+    it('should display input for exam number', () => {
+        cy.get('input[type="text"]').should('exist');
+    });
 })
 
-it('should display header on search page', () => {
-    cy.get('#logo').should("be.visible");
-})
+describe('verify searchbar functionality', () => {
+    it('should display results when searching with valid exam number', () => {
+        cy.get('input[type="text"]').type('11000');
+        cy.get('button').click();
+        cy.contains('Privatrecht');
+    });
 
-it('should display label for searchbar', () => {
-    cy.get('body').then((body) => {
-       cy.contains("Prüfungsnummer eingeben:")
-    })
-});
+    it('should display error when no exams were found', () => {
+        cy.get('input[type="text"]').type('99999');
+        cy.get('button').click();
+        cy.contains('Keine Prüfungen für die Prüfungslaufnummer 99999 gefunden');
+    });
 
-it('should display input for exam number', () => {
-    cy.get('input[type="text"]').should('exist');
-});
-
-it('should display results when searching with valid exam number', () => {
-    cy.get('input[type="text"]').type('11000');
-    cy.get('button').click();
-    cy.contains('Privatrecht');
-});
-
-it('should display error when no exams were found', () => {
-    cy.get('input[type="text"]').type('99999');
-    cy.get('button').click();
-    cy.contains('Keine Prüfungen für die Prüfungslaufnummer 99999 gefunden');
-});
-
-it('should display error when exam number is too short', () => {
-    cy.get('input[type="text"]').type('123');
-    cy.get('button').click();
-    cy.contains('Prüfungsnummer muss aus genau 5 Ziffern bestehen.');
+    it('should display error when exam number is too short', () => {
+        cy.get('input[type="text"]').type('123');
+        cy.get('button').click();
+        cy.contains('Prüfungsnummer muss aus genau 5 Ziffern bestehen.');
 });
 
 it('should display error when input consists of letters', () => {
     cy.get('input[type="text"]').type('asdf');
     cy.get('[data-testid=\"submit-button\"]').click();
     cy.contains('Prüfungsnummer muss aus genau 5 Ziffern bestehen.');
-});
+    });
 
-it('should display download as ZIP button when input is valid', () => {
-    cy.get('input[type="text"]').type('11000');
-    cy.get('button').click();
-    cy.contains('Alle als ZIP herunterladen');
-});
+    it('should not be able to input more than 5 characters', () => {
+        cy.get('input[type="text"]').type('110000').should('have.value', '11000');
+    });
 
-it('should not display ZIP download when exam number is invalid', () => {
-    cy.get('input[type="text"]').type('123');
-    cy.get('button').click();
-    cy.get('Alle als ZIP herunterladen').should('not.exist');
-});
-
-it('should download files with correct name when link is clicked', () => {
-    cy.get('input[type="text"]').type('11000');
-    cy.get('button').click();
-    const subjects = ['Handels und Gesellschaftsrecht', 'Privatrecht', 'Strafrecht']
-
-    for (let i = 0; i < subjects.length; i++) {
-        cy.contains(subjects[i]).click();
-        cy.readFile(`cypress/downloads/${subjects[i]}.pdf`).should('exist');
-    }
-});
-
-it('should show downloadable files with name of subject-folder they are in', () => {
-    cy.get('input[type="text"]').type('11000');
-    cy.get('button').click();
-    cy.contains('Handels und Gesellschaftsrecht');
-    cy.contains('Privatrecht');
-    cy.contains('Strafrecht');
-    cy.contains('Öffentliches Recht')
-});
-
-it('should not be disabled if number is in input', () => {
-    cy.get('input[type="text"]');
-    cy.get('button').should('not.be.disabled');
-});
-
-it('should not be able to input more than 5 characters', () => {
-    cy.get('input[type="text"]').type('110000').should('have.value', '11000');
-});
-
-it('should show files in alphabetical order', () => {
+    it('should display download as ZIP button when input is valid', () => {
         cy.get('input[type="text"]').type('11000');
         cy.get('button').click();
-    cy.get('.exam-files').then(files => {
-        const fileNames = [...files].map(file => file.innerText);
-        const sortedFileNames = ["Handels und Gesellschaftsrecht", "Öffentliches Recht", "Privatrecht", "Strafrecht"];
-        expect(fileNames).to.deep.equal(sortedFileNames)
-    })
+        cy.contains('Alle als ZIP herunterladen');
+    });
+
+    it('should not display ZIP download when exam number is invalid', () => {
+        cy.get('input[type="text"]').type('123');
+        cy.get('button').click();
+        cy.get('Alle als ZIP herunterladen').should('not.exist');
+    });
+})
+
+describe('verify download functionality', () => {
+    it('should show downloadable files with name of subject-folder they are in', () => {
+        cy.get('input[type="text"]').type('11000');
+        cy.get('button').click();
+        cy.contains('Handels und Gesellschaftsrecht');
+        cy.contains('Privatrecht');
+        cy.contains('Strafrecht');
+        cy.contains('Öffentliches Recht')
+    });
+
+    it('should show files in alphabetical order', () => {
+        cy.get('input[type="text"]').type('11000');
+        cy.get('button').click();
+        cy.get('.exam-files').then(files => {
+            const fileNames = [...files].map(file => file.innerText);
+            const sortedFileNames = ["Handels und Gesellschaftsrecht", "Öffentliches Recht", "Privatrecht", "Strafrecht"];
+            expect(fileNames).to.deep.equal(sortedFileNames)
+        })
+    });
+
+    it('should download files with correct name when link is clicked', () => {
+        cy.get('input[type="text"]').type('11000');
+        cy.get('button').click();
+        const subjects = ['Handels und Gesellschaftsrecht', 'Privatrecht', 'Strafrecht']
+
+        for (let i = 0; i < subjects.length; i++) {
+            cy.contains(subjects[i]).click();
+            cy.readFile(`cypress/downloads/${subjects[i]}.pdf`).should('exist');
+        }
+    });
 });
