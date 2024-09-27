5d9468a4fae1988cc9069394b75ab59dc8bae531
Miguel Lehmann <lehmann@puzzle.ch>
2024-09-27 10:40:07 +0200

    remove usless test

--- src/test/e2e/cypress/e2e/index.cy.js
+++ src/test/e2e/cypress/e2e/index.cy.js
@@ -14,20 +14,6 @@ it('should show greeting message on index page', () => {
     })
 })
 
-it('should open swissuniversities.ch on click', async () => {
-    cy.intercept({
-        method: 'GET',
-        url: 'https://www.swissuniversities.ch/'
-    }).as('redirectRequest');
-
-    cy.get("footer").find("img").parent()
-        .should('have.attr', 'href', 'https://www.swissuniversities.ch/')
-        .and('have.attr', 'target', '_blank')
-        .invoke('attr', 'target', '_self')
-        .click()
-    cy.wait('@redirectRequest').its('response.statusCode').should('eq', 200);
-})
-
 it('should display header on index page', () => {
     cy.get('#logo').should("be.visible");
 })
