de84cbe11a953e19f111e5f78172d2be6cf353eb
Manuel <109959820+ManuelMoeri@users.noreply.github.com>
2024-10-01 15:15:02 +0200

    Feature/104 No console errors (#108)
    * Remove permission policys that generated erros
    
    * Add data for images in csp config to allow bootstrap to load pictures
    
    * Extend security config

--- src/main/java/ch/puzzle/eft/SecurityConfig.java
+++ src/main/java/ch/puzzle/eft/SecurityConfig.java
@@ -18,8 +18,8 @@ import static org.springframework.security.web.header.writers.CrossOriginEmbedde
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private static final String CSP_CONFIG = "default-src 'self';" + "script-src 'strict-dynamic' 'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz';" + "style-src 'self';" + "img-src 'self'; " + "font-src 'self';" + "object-src 'none';" + "base-uri 'self';" + "form-action 'self';" + "frame-ancestors 'none'; ";
-    private static final String PERMISSION_POLICY = "accelerometer=()," + "ambient-light-sensor=()," + "autoplay=(self <origin>)," + "battery=()," + "camera=()," + "display-capture=()," + "document-domain=()," + "encrypted-media=()," + "fullscreen=()," + "gamepad=()," + "geolocation=()," + "gyroscope=()," + "magnetometer=()," + "microphone=()," + "picture-in-picture=()," + "speaker-selection=()," + "usb=()," + "screen-wake-lock=()," + "xr-spatial-tracking=()";
+    private static final String CSP_CONFIG = "base-uri 'self';" + "connect-src 'none';" + "default-src 'self';" + "script-src 'strict-dynamic' 'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz';" + "style-src 'self';" + "img-src 'self' data:; " + "font-src 'self';" + "object-src 'none';" + "form-action 'self';" + "frame-ancestors 'none'; " + "frame-src 'none';"; //TODO: restrict upgrade-insecure-requests
+    private static final String PERMISSION_POLICY = "accelerometer=()," + "attribution-reporting=()," + "autoplay=()," + "camera=()," + "compute-pressure=()," + "display-capture=()," + "encrypted-media=()," + "fullscreen=()," + "gamepad=()," + "geolocation=()," + "gyroscope=()," + "hid=()," + "identity-credentials-get=()," + "idle-detection=()," + "local-fonts=()," + "magnetometer=()," + "microphone=()," + "midi=()," + "otp-credentials=()," + "payment=()," + "picture-in-picture=()," + "publickey-credentials-create=()," + "publickey-credentials-get=()," + "screen-wake-lock=()," + "serial=()," + "storage-access=()," + "usb=()," + "window-management=()," + "xr-spatial-tracking=()";
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
