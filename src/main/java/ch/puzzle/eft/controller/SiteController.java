7ba30a293c7e883521e4fdf23fbc3e6a456e1fc2
Miguel Lehmann <lehmann@puzzle.ch>
2024-10-03 10:22:16 +0200

    use duration to set cookie max age

--- src/main/java/ch/puzzle/eft/controller/SiteController.java
+++ src/main/java/ch/puzzle/eft/controller/SiteController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.Duration;
 import java.util.Objects;
 
 @Controller
@@ -58,7 +59,8 @@ public class SiteController {
     @PostMapping("/")
     public ResponseEntity<String> acceptCookies(Model model) {
         ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie.from("cookie-consent", "true");
-        cookieBuilder.maxAge((long) 60 * 60 * 24 * 365);
+        cookieBuilder.maxAge(Duration.ofDays(365)
+                                     .toSeconds());
         cookieBuilder.sameSite("Strict");
         cookieBuilder.httpOnly(true);
         //        cookieBuilder.secure(true); TODO: do as soon as login is in main
