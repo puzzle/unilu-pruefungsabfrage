309a2b687a7d99e3ffa0edd7a555e7b0fe97afa3
Jannik Pulfer <pulfer@puzzle.ch>
2024-10-16 11:33:24 +0200

    Remove unauthorized urls and now require login for all urls

--- src/main/java/ch/puzzle/eft/config/SecurityConfig.java
+++ src/main/java/ch/puzzle/eft/config/SecurityConfig.java
@@ -33,8 +33,9 @@ public class SecurityConfig {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
     private static final String CSP_CONFIG = "base-uri 'self';" + "connect-src 'none';" + "default-src 'self';" + "script-src 'strict-dynamic' 'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz';" + "style-src 'self';" + "img-src 'self' data:; " + "font-src 'self';" + "object-src 'none';" + "form-action 'self';" + "frame-ancestors 'none'; " + "frame-src 'none';"; //TODO: restrict upgrade-insecure-requests
     private static final String PERMISSION_POLICY = "accelerometer=()," + "attribution-reporting=()," + "autoplay=()," + "camera=()," + "compute-pressure=()," + "display-capture=()," + "encrypted-media=()," + "fullscreen=()," + "gamepad=()," + "geolocation=()," + "gyroscope=()," + "hid=()," + "identity-credentials-get=()," + "idle-detection=()," + "local-fonts=()," + "magnetometer=()," + "microphone=()," + "midi=()," + "otp-credentials=()," + "payment=()," + "picture-in-picture=()," + "publickey-credentials-create=()," + "publickey-credentials-get=()," + "screen-wake-lock=()," + "serial=()," + "storage-access=()," + "usb=()," + "window-management=()," + "xr-spatial-tracking=()";
-    @Value("${server.unauthorized.urls:/}")
-    private String unauthorizedUrls;
+
+    @Value("${test.mock.principal:false}")
+    private boolean mockPrincipal;
 
     @Bean
     public AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> userDetailsService() {
@@ -66,7 +67,7 @@ public class SecurityConfig {
 
     @Bean
     public RequestAttributeAuthenticationFilter authenticationFilter(AuthenticationManager authenticationManager) {
-        RequestAttributeAuthenticationFilter authenticationFilter = new ShibbolethRequestAuthenticationFilter();
+        RequestAttributeAuthenticationFilter authenticationFilter = new ShibbolethRequestAuthenticationFilter(mockPrincipal);
         authenticationFilter.setAuthenticationManager(authenticationManager);
         authenticationFilter.setContinueFilterChainOnUnsuccessfulAuthentication(false);
         authenticationFilter.setCheckForPrincipalChanges(false);
@@ -80,8 +81,6 @@ public class SecurityConfig {
         http.cors(Customizer.withDefaults())
             .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**") // pre-authenticated by Shibboleth
                                                          .authenticated()
-                                                         .requestMatchers(unauthorizedUrls.split(" "))
-                                                         .permitAll()
                                                          .anyRequest()
                                                          .denyAll())
             .addFilterBefore(authenticationFilter, SecurityContextHolderFilter.class)
