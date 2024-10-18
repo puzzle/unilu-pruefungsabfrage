7a9f898e8ef11c70bf433063716a0cb29cf311d1
Jannik Pulfer <pulfer@puzzle.ch>
2024-10-18 08:13:57 +0200

    Extend authentication user details test to test for more default values

--- src/test/java/ch/puzzle/eft/config/AuthenticationUserDetailsTest.java
+++ src/test/java/ch/puzzle/eft/config/AuthenticationUserDetailsTest.java
@@ -1,17 +1,22 @@
 package ch.puzzle.eft.config;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class AuthenticationUserDetailsTest {
 
     @Test
-    void shouldReturnStringNotNull() {
+    void shouldReturnCorrectDefaultValue() {
         AuthenticationUserDetails details = new AuthenticationUserDetails(new AuthenticationUser("", ""));
 
         String result = details.getUsername();
 
         assertEquals("n/a", result);
+        assertNull(details.getPassword());
+        assertEquals(List.of(), details.getAuthorities());
     }
 }
