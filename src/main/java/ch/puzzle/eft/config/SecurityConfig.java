3340ea42401eacb88a4ad9d4233462c40d1d0bf9
Jannik Pulfer <pulfer@puzzle.ch>
2024-10-21 11:37:00 +0200

    Remove unused logger from security config

--- src/main/java/ch/puzzle/eft/config/SecurityConfig.java
+++ src/main/java/ch/puzzle/eft/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import java.security.Principal;
 import java.security.Security;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,7 +26,6 @@ import static org.springframework.security.web.header.writers.CrossOriginEmbedde
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
     private static final String CSP_CONFIG = "base-uri 'self';" + "connect-src 'none';" + "default-src 'self';" + "script-src 'strict-dynamic' 'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz';" + "style-src 'self';" + "img-src 'self' data:; " + "font-src 'self';" + "object-src 'none';" + "form-action 'self';" + "frame-ancestors 'none';" + "frame-src 'none';";
     private static final String PERMISSION_POLICY = "accelerometer=()," + "attribution-reporting=()," + "autoplay=()," + "camera=()," + "compute-pressure=()," + "display-capture=()," + "encrypted-media=()," + "fullscreen=()," + "gamepad=()," + "geolocation=()," + "gyroscope=()," + "hid=()," + "identity-credentials-get=()," + "idle-detection=()," + "local-fonts=()," + "magnetometer=()," + "microphone=()," + "midi=()," + "otp-credentials=()," + "payment=()," + "picture-in-picture=()," + "publickey-credentials-create=()," + "publickey-credentials-get=()," + "screen-wake-lock=()," + "serial=()," + "storage-access=()," + "usb=()," + "window-management=()," + "xr-spatial-tracking=()";
 
