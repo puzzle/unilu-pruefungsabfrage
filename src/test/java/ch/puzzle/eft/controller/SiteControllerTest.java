4e2c6d59e91ef8b8fe14cbcdb749e378044188d7
Jannik Pulfer <109959802+RandomTannenbaum@users.noreply.github.com>
2024-10-23 13:10:31 +0200

    Set login cookie to secure and test that, add env var that sets secure cookie to false for cypress tests (#159)

--- src/test/java/ch/puzzle/eft/controller/SiteControllerTest.java
+++ src/test/java/ch/puzzle/eft/controller/SiteControllerTest.java
@@ -110,7 +110,7 @@ class SiteControllerTest {
         Assertions.assertTrue(cookieHeader.contains("cookie-consent=true;"));
         Assertions.assertTrue(cookieHeader.contains("Max-Age=31536000;"));
         Assertions.assertTrue(cookieHeader.contains("HttpOnly;"));
-        //        Assertions.assertTrue(cookieHeader.contains("Secure;")); TODO: do as soon as login is in main
+        Assertions.assertTrue(cookieHeader.contains("Secure;"));
     }
 
     @Test
