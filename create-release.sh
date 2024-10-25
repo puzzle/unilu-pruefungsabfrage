de98a15736946ca275f5bbeb463574fac20a1bd0
Giannin <puzzle@giannin.ch>
2024-10-25 10:13:34 +0200

    Change action to install binary to a more supported one

--- create-release.sh
+++ create-release.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 semver_version=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout | cut -d '-' -f 1)
 short_commit_hash=$(git rev-parse --short HEAD)
 release_tag="$semver_version.$short_commit_hash"
