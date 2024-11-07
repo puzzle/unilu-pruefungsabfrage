8e038815cafa4bc85d1a010674f6b7eab8fac0d0
Giannin <puzzle@giannin.ch>
2024-11-07 08:42:05 +0100

    AJP address is now set to localhost

--- src/main/java/ch/puzzle/eft/config/AppServerConfig.java
+++ src/main/java/ch/puzzle/eft/config/AppServerConfig.java
@@ -1,9 +1,12 @@
 package ch.puzzle.eft.config;
 
-import java.net.InetSocketAddress;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -12,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppServerConfig {
-
+    private static final Logger logger = LoggerFactory.getLogger(AppServerConfig.class);
     private static final String PROTOCOL = "AJP/1.3";
 
     @Value("${server.ajp.protocol:https}")
@@ -43,8 +46,21 @@ public class AppServerConfig {
         protocol.setTomcatAuthentication(false);
         protocol.setAllowedRequestAttributesPattern(".{1,}"); // should be "AJP_"
         protocol.setPacketSize(ajpPacketSize);
-        protocol.setAddress(new InetSocketAddress(0).getAddress()); // necessary because without it AJP won't work over Docker bridge networks
+        protocol.setAddress(getLocalhost());
 
         return connector;
+    }
+
+    // necessary because without it AJP won't work over Docker bridge networks
+    // https://stackoverflow.com/questions/61524263/ajp-in-containerised-spring-boot-app-doesnt-work
+    private InetAddress getLocalhost() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            logger.debug("Setting address for AJP protocol to '{}'", localhost);
+            return localhost;
+        } catch (UnknownHostException e) {
+            logger.error("Cannot determine localhost address, which is needed for the AJP connector");
+            throw new RuntimeException(e);
+        }
     }
 }
